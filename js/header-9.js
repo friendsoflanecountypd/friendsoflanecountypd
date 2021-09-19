722044f91fe60128113a326de6ed0c2ab54e586a
Myra Lukens <myra_lukens@apple.com>
2021-09-18 18:27:59 -0700

    hamburger

--- js/header-9.js
+++ js/header-9.js
@@ -1,14 +1,18 @@
-let navToggle = document.querySelector(".nav__toggle");
 let navWrapper = document.querySelector(".nav__wrapper");
+let navToggle = document.querySelector(".nav__toggle");
 
-navToggle.addEventListener("click", function () {
-  if (navWrapper.classList.contains("active")) {
-    this.setAttribute("aria-expanded", "false");
-    this.setAttribute("aria-label", "menu");
-    navWrapper.classList.remove("active");
-  } else {
-    navWrapper.classList.add("active");
-    this.setAttribute("aria-label", "close menu");
-    this.setAttribute("aria-expanded", "true");
-  }
+$(document).ready(function(){
+  $('#nav-icon4').click(function(){
+    $(this).toggleClass('open');
+
+    if (navWrapper.classList.contains("active")) {
+      this.setAttribute("aria-expanded", "false");
+      this.setAttribute("aria-label", "menu");
+      navWrapper.classList.remove("active");
+    } else {
+      navWrapper.classList.add("active");
+      this.setAttribute("aria-label", "close menu");
+      this.setAttribute("aria-expanded", "true");
+    }
+  });
 });
